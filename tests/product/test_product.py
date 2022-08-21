eb51e167ffcf65bfa50b8af4b2f23fc8bc609435
Felipe Matheus <fmatheusog@gmail.coM>
2022-08-21 14:34:04 -0300

    test: implement Product class constructor test

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,29 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    id = 1
+    nome_produto = "nome_produto"
+    nome_empresa = "nome_empresa"
+    data_fabricacao = "data_fabricacao"
+    data_validade = "data_validade"
+    numero_serie = "numero_serie"
+    instrucoes = "instrucoes"
+
+    product = Product(
+        id,
+        nome_produto,
+        nome_empresa,
+        data_fabricacao,
+        data_validade,
+        numero_serie,
+        instrucoes
+        )
+
+    assert (product.id) == 1
+    assert (product.nome_do_produto) == nome_produto
+    assert (product.nome_da_empresa) == nome_empresa
+    assert (product.data_de_fabricacao) == data_fabricacao
+    assert (product.data_de_validade) == data_validade
+    assert (product.numero_de_serie) == numero_serie
+    assert (product.instrucoes_de_armazenamento) == instrucoes
