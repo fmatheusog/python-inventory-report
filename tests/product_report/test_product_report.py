2b6f19ff197bdb5b792f78b4c117821760b980df
Felipe Matheus <fmatheusog@gmail.coM>
2022-08-21 17:52:47 -0300

    test: implement Product class repr test

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,29 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    id = 1
+    nome_produto = "nome_produto"
+    nome_empresa = "nome_empresa"
+    data_fabricacao = "data_fabricacao"
+    data_validade = "data_validade"
+    numero_serie = "numero_serie"
+    instrucoes = "instrucoes"
+
+    product = Product(
+        id,
+        nome_produto,
+        nome_empresa,
+        data_fabricacao,
+        data_validade,
+        numero_serie,
+        instrucoes
+        )
+
+    assert (repr(product)) == (
+        f"O produto {nome_produto}"
+        f" fabricado em {data_fabricacao}"
+        f" por {nome_empresa} com validade"
+        f" até {data_validade}"
+        f" precisa ser armazenado {instrucoes}."
+    )
